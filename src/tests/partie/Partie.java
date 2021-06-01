1f3387d18732f40227965a636819c5536ef4f2ab
Hoest Edouard <edouard_hoest@ens.univ-artois.fr>
2021-06-01 19:52:21 +0000

    Edouard

--- src/tests/partie/Partie.java
+++ src/tests/partie/Partie.java
@@ -1,77 +1,115 @@
-package tests;
-import joueurs.Joueur;
-import composants.Plateau;
-import composants.Piece;
+package partie;
+
 import composants.Objet;
-import partie.ElementsPartie;
+import composants.Piece;
+import composants.Plateau;
 import grafix.interfaceGraphique.IG;
+import joueurs.Joueur;
+import joueurs.JoueurOrdinateur;
 
-public class TestElementsPartie {
-    public static void main(String[] args){
-
-
-        Object parametres[];
-            parametres=IG.saisirParametres(); // On ouvre la fenêtre de paramétrage pour la saisie
-            
-            // Création de la fenêtre de jeu 
-            int nbJoueurs=((Integer)parametres[0]).intValue(); // Récupération du nombre de joueurs
-            IG.creerFenetreJeu("- TestElementsPartie",nbJoueurs);            //creation et mise en place des pieces du plateau
-            Plateau plateau = new Plateau();
-            Piece pieceHorsPlateau=plateau.placerPiecesAleatoierment();
-            Joueur joueurs[]=Joueur.nouveauxJoueurs(parametres);
-            Objet[] tab = Objet.nouveauxObjets();
-            ElementsPartie elementsPartie=new ElementsPartie(joueurs);
-            for (int i=0; i<7; i++){
-                for (int j=0; j<7; j++){
-                    IG.changerPiecePlateau(i, j, plateau.getPiece(i,j).getModelePiece(), plateau.getPiece(i,j).getOrientationPiece());
-                }
-            }
-            IG.changerPieceHorsPlateau(pieceHorsPlateau.getModelePiece(), pieceHorsPlateau.getOrientationPiece());
-            
-            // creation et placement des joueurs
-            if (nbJoueurs==2){
-                joueurs[0].setPosition(0, 0);
-                IG.placerJoueurSurPlateau(0, 0, 0);
-                joueurs[1].setPosition(0, 6);
-                IG.placerJoueurSurPlateau(1, 0, 6);
-            }else{
-                joueurs[0].setPosition(0, 0);
-                IG.placerJoueurSurPlateau(0, 0, 0);
-                joueurs[1].setPosition(0, 6);
-                IG.placerJoueurSurPlateau(1, 0, 6);
-                joueurs[2].setPosition(6, 6);
-                IG.placerJoueurSurPlateau(2, 6, 6);
-            }
-            // mise en place des image/nom/categorie des joueurs
-            for (int i=0; i<nbJoueurs;i++){
-                int numImageJoueur=(joueurs[i].getNumeroImagePersonnage());
-                String nomJoueur=(joueurs[i].getNomJoueur());
-                String categorieJoueur=(joueurs[i].getCategorie());
-                IG.changerNomJoueur(i, nomJoueur+" ("+categorieJoueur+")");
-                IG.changerImageJoueur(i,numImageJoueur);
-            }
-
-            
-            for (int i = 0; i<tab.length; i++){
-                IG.placerObjetPlateau(tab[i].getNumeroObjet(),tab[i].getPosconnePlateau(), tab[i].getPoslePlateau());
-            }
-            
+public class Partie {
+	static double version=0.0;
 
 
-            IG.rendreVisibleFenetreJeu();  // On rend visible la fenêtre de jeu
+	private ElementsPartie elementsPartie; // Les éléments de la partie.
 
-            // Affichage d'un message
-            String message[]={
-                "",
-                "Cliquer pour continuer ...",
-                ""
-            };
-            IG.afficherMessage(message); // On change de message de la fenêtre de jeu
-            IG.miseAJourAffichage(); // On effectue le rafraichissement de la fenêtre de jeu
-            IG.attendreClic();
+	/**
+	 * 
+	 * A Faire (01/06/2021 EH Finalisée)
+	 * 
+	 * Constructeur permettant de créer et d'initialiser une nouvelle partie.
+	 */
+	public Partie(){
+		// Initialisation de la partie
+		parametrerEtInitialiser();
 
-            IG.attendreClic();
-            IG.fermerFenetreJeu();
-            System.exit(0);
-    }
+		// On affiche l'ensemble des éléments
+		
+        //creation et mise en place des pieces du plateau
+		for (int i=0; i<7; i++){
+			for (int j=0; j<7; j++){
+				IG.changerPiecePlateau(i, j, elementsPartie.getPlateau().getPiece(i,j).getModelePiece(), elementsPartie.getPlateau().getPiece(i,j).getOrientationPiece());
+			}
+		}
+		IG.changerPieceHorsPlateau(elementsPartie.getPieceLibre().getModelePiece(), elementsPartie.getPieceLibre().getOrientationPiece());
+		
+		// creation et placement des joueurs
+		int nbJoueurs = elementsPartie.getNombreJoueurs();
+		Joueur[] joueurs = elementsPartie.getJoueurs();
+		if (nbJoueurs==2){
+			joueurs[0].setPosition(0, 0);
+			IG.placerJoueurSurPlateau(0, 0, 0);
+			joueurs[1].setPosition(0, 6);
+			IG.placerJoueurSurPlateau(1, 0, 6);
+		}else{
+			joueurs[0].setPosition(0, 0);
+			IG.placerJoueurSurPlateau(0, 0, 0);
+			joueurs[1].setPosition(0, 6);
+			IG.placerJoueurSurPlateau(1, 0, 6);
+			joueurs[2].setPosition(6, 6);
+			IG.placerJoueurSurPlateau(2, 6, 6);
+		}
+		// mise en place des image/nom/categorie des joueurs
+		for (int i=0; i<nbJoueurs;i++){
+			int numImageJoueur=(joueurs[i].getNumeroImagePersonnage());
+			String nomJoueur=(joueurs[i].getNomJoueur());
+			String categorieJoueur=(joueurs[i].getCategorie());
+			IG.changerNomJoueur(i, nomJoueur+" ("+categorieJoueur+")");
+			IG.changerImageJoueur(i,numImageJoueur);
+		}
+
+		Objet[] tabObjet = elementsPartie.getObjets();
+		
+		for (int i = 0; i<tabObjet.length; i++){
+			IG.placerObjetPlateau(tabObjet[i].getNumeroObjet(),tabObjet[i].getPosconnePlateau(), tabObjet[i].getPoslePlateau());
+		}
+
+		for(int j= 0; j<(tabObjet.length/nbJoueurs); j++ ){
+			IG.changerObjetJoueur(0,joueurs[0].getObjetsJoueur()[j].getNumeroObjet(), j);
+			IG.changerObjetJoueur(1,joueurs[1].getObjetsJoueur()[j].getNumeroObjet(), j);
+			IG.changerObjetJoueur(2,joueurs[2].getObjetsJoueur()[j].getNumeroObjet(), j);
+		}
+
+		IG.rendreVisibleFenetreJeu();
+	}
+
+	/**
+	 * Méthode permettant de paramètrer et initialiser les éléments de la partie.
+	 */
+	private void parametrerEtInitialiser(){
+		// Saisie des différents paramètres
+		Object parametresJeu[];
+		parametresJeu=IG.saisirParametres();
+		int nombreJoueurs=((Integer)parametresJeu[0]).intValue();
+		IG.creerFenetreJeu("- Version "+version, nombreJoueurs);
+		// Création des joueurs
+		Joueur joueurs[]=Joueur.nouveauxJoueurs(parametresJeu);
+		// Création des éléments de la partie
+		elementsPartie=new ElementsPartie(joueurs);
+	}
+
+
+	/**
+	 * 
+	 * A Faire (Quand Qui Statut)
+	 * 
+	 * Méthode permettant de lancer une partie.
+	 */
+	public void lancer(){
+		// A Compléter
+	}
+
+	/**
+	 * 
+	 * Programme principal permettant de lancer le jeu.
+	 * 
+	 * @param args Les arguments du programmes.
+	 */
+	public static void main(String[] args) {
+		while(true){
+			Partie partie=new Partie();
+			partie.lancer();
+		}
+	}
+
 }
