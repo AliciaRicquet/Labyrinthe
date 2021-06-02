0fea50c10c34f86dbb56e4a74d40425df211f097
Edouard Hoest <edouard_hoest@ens.univ-artois.fr>
2021-06-02 15:59:08 +0200

    Fin TestElementPartie, constructeur Partie + debut boucle while methode lancer

--- src/tests/partie/Partie.java
+++ src/tests/partie/Partie.java
@@ -36,6 +36,7 @@ public class Partie {
 		// creation et placement des joueurs
 		int nbJoueurs = elementsPartie.getNombreJoueurs();
 		Joueur[] joueurs = elementsPartie.getJoueurs();
+		
 		if (nbJoueurs==2){
 			joueurs[0].setPosition(0, 0);
 			IG.placerJoueurSurPlateau(0, 0, 0);
@@ -61,7 +62,7 @@ public class Partie {
 		Objet[] tabObjet = elementsPartie.getObjets();
 		
 		for (int i = 0; i<tabObjet.length; i++){
-			IG.placerObjetPlateau(tabObjet[i].getNumeroObjet(),tabObjet[i].getPosconnePlateau(), tabObjet[i].getPoslePlateau());
+			IG.placerObjetPlateau(tabObjet[i].getNumeroObjet(),tabObjet[i].getPoslePlateau(), tabObjet[i].getPosconnePlateau());
 		}
 
 		for(int j= 0; j<(tabObjet.length/nbJoueurs); j++ ){
@@ -91,12 +92,17 @@ public class Partie {
 
 	/**
 	 * 
-	 * A Faire (Quand Qui Statut)
+	 * A Faire (02/06/2021 SA/IB/AR EnCours)
 	 * 
 	 * Méthode permettant de lancer une partie.
 	 */
 	public void lancer(){
-		// A Compléter
+		int nbJoueurs = elementsPartie.getNombreJoueurs();
+		Joueur[] joueurs = elementsPartie.getJoueurs();
+		Objet[] tabObjet = elementsPartie.getObjets();
+		while (joueurs[0].getNombreObjetsRecuperes()!= tabObjet.length/nbJoueurs && joueurs[1].getNombreObjetsRecuperes()!= tabObjet.length/nbJoueurs && joueurs[2].getNombreObjetsRecuperes()!= tabObjet.length/nbJoueurs){
+			
+		}
 	}
 
 	/**
