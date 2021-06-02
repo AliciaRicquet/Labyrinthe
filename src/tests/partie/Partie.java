63fde35ce38cff70b98717e2a5bf3171489439fa
Edouard Hoest <edouard_hoest@ens.univ-artois.fr>
2021-06-02 19:35:27 +0200

    etape 5(quelques bugs)

--- src/tests/partie/Partie.java
+++ src/tests/partie/Partie.java
@@ -65,13 +65,33 @@ public class Partie {
 			IG.placerObjetPlateau(tabObjet[i].getNumeroObjet(),tabObjet[i].getPoslePlateau(), tabObjet[i].getPosconnePlateau());
 		}
 
-		for(int j= 0; j<(tabObjet.length/nbJoueurs); j++ ){
-			IG.changerObjetJoueur(0,joueurs[0].getObjetsJoueur()[j].getNumeroObjet(), j);
-			IG.changerObjetJoueur(1,joueurs[1].getObjetsJoueur()[j].getNumeroObjet(), j);
-			IG.changerObjetJoueur(2,joueurs[2].getObjetsJoueur()[j].getNumeroObjet(), j);
+		if (nbJoueurs==3){
+			for(int j= 0; j<(tabObjet.length/nbJoueurs); j++ ){
+				IG.changerObjetJoueur(0,joueurs[0].getObjetsJoueur()[j].getNumeroObjet(), j);
+				IG.changerObjetJoueur(1,joueurs[1].getObjetsJoueur()[j].getNumeroObjet(), j);
+				IG.changerObjetJoueur(2,joueurs[2].getObjetsJoueur()[j].getNumeroObjet(), j);
+			}
+		}else{
+			for(int j= 0; j<(tabObjet.length/nbJoueurs); j++ ){
+				IG.changerObjetJoueur(0,joueurs[0].getObjetsJoueur()[j].getNumeroObjet(), j);
+				IG.changerObjetJoueur(1,joueurs[1].getObjetsJoueur()[j].getNumeroObjet(), j);
+			}
 		}
+		
 
+	
 		IG.rendreVisibleFenetreJeu();
+
+		String messageDebutGame[]={
+			"",
+			"Bonjour! Chers camarades, ", 
+			"",
+			"Cliquez pour lancer la partie",
+			""
+		};
+		IG.afficherMessage(messageDebutGame);
+		IG.miseAJourAffichage();
+		IG.attendreClic();
 	}
 
 	/**
@@ -100,10 +120,135 @@ public class Partie {
 		int nbJoueurs = elementsPartie.getNombreJoueurs();
 		Joueur[] joueurs = elementsPartie.getJoueurs();
 		Objet[] tabObjet = elementsPartie.getObjets();
-		while (joueurs[0].getNombreObjetsRecuperes()!= tabObjet.length/nbJoueurs && joueurs[1].getNombreObjetsRecuperes()!= tabObjet.length/nbJoueurs && joueurs[2].getNombreObjetsRecuperes()!= tabObjet.length/nbJoueurs){
-			
+		Plateau plateau = elementsPartie.getPlateau();
+		boolean gagnant = false;
+
+		while (gagnant == false){
+			for(int numJoueurs = 0; numJoueurs < nbJoueurs; numJoueurs ++){
+				String messageJoueur[]={
+				"",
+				"Au tour de " + joueurs[numJoueurs].getNomJoueur(),
+				"",
+				"Cliquez pour continuer ...",
+				""
+				};
+				IG.afficherMessage(messageJoueur);
+				IG.changerJoueurSelectionne(numJoueurs);
+				IG.changerObjetSelectionne(tabObjet[joueurs[numJoueurs].getObjetsJoueur()[joueurs[numJoueurs].getNombreObjetsRecuperes()].getNumeroObjet()].getNumeroObjet());
+				IG.miseAJourAffichage();
+				IG.attendreClic();
+
+				
+				String messagePieceHorsPlateau[]={
+					"",
+					"Choisissez une orientation", 
+					"de la piece libre",
+					"Ensuite, selectionnez une flèche",
+					""
+				};
+				IG.afficherMessage(messagePieceHorsPlateau);
+				IG.miseAJourAffichage();
+
+				int choix = IG.attendreChoixEntree();
+				elementsPartie.getPieceLibre().setOrientation(IG.recupererOrientationPieceHorsPlateau());
+
+				elementsPartie.insertionPieceLibre(choix);
+				for (int i = 0; i<tabObjet.length; i++){
+					for(int ligne=0; ligne<7;ligne++){
+						for (int colonne=0; colonne<7;colonne++){
+							if (tabObjet[i].getPoslePlateau()!=ligne && tabObjet[i].getPosconnePlateau()!=colonne){
+								IG.enleverObjetPlateau(ligne, colonne);
+							}
+						}
+					}
+				}
+				for (int i = 0; i<tabObjet.length; i++){
+					IG.placerObjetPlateau(tabObjet[i].getNumeroObjet(),tabObjet[i].getPoslePlateau(), tabObjet[i].getPosconnePlateau());
+				}
+				IG.miseAJourAffichage();
+				
+				for (int i=0; i<7; i++){
+					for (int j=0; j<7; j++){
+						IG.changerPiecePlateau(i, j, elementsPartie.getPlateau().getPiece(i,j).getModelePiece(), elementsPartie.getPlateau().getPiece(i,j).getOrientationPiece());
+					}
+				}
+				IG.changerPieceHorsPlateau(elementsPartie.getPieceLibre().getModelePiece(), elementsPartie.getPieceLibre().getOrientationPiece());
+				for (int i=0; i<nbJoueurs;i++){
+					IG.placerJoueurSurPlateau(i, joueurs[i].getPosLigne(), joueurs[i].getPosColonne());
+					
+				}
+
+				IG.miseAJourAffichage();
+
+				String messageDeplacement[]={
+					"",
+					"Selectionnez une case d'arrivé",
+					""
+				};
+				IG.afficherMessage(messageDeplacement);
+				IG.miseAJourAffichage();
+
+				if(joueurs[numJoueurs].getCategorie()=="ordinateur"){
+					IG.placerBilleSurPlateau(joueurs[numJoueurs].getPosLigne(), joueurs[numJoueurs].getPosColonne(), 1, 1, 0);
+				}else{
+					int[] casArr = new int[2];
+					casArr = joueurs[numJoueurs].choisirCaseArrivee(null);
+	
+					int[][] resultat = plateau.calculeChemin(joueurs[numJoueurs].getPosLigne(), joueurs[numJoueurs].getPosColonne(), casArr[0], casArr[1]);
+					while(resultat==null){
+						casArr = joueurs[numJoueurs].choisirCaseArrivee(null);
+						resultat = plateau.calculeChemin(joueurs[numJoueurs].getPosLigne(), joueurs[numJoueurs].getPosColonne(), casArr[0], casArr[1]);
+					}
+					for (int x=0;x<7;x++){
+						for (int j=0;j<7;j++){
+							if (resultat!=null){
+								IG.placerJoueurSurPlateau(joueurs[numJoueurs].getNumJoueur(), resultat[resultat.length-1][0], resultat[resultat.length-1][1]);
+								for (int k=0; k<resultat.length;k++){
+									IG.placerBilleSurPlateau(resultat[k][0], resultat[k][1], 1, 1, numJoueurs);
+								}
+							}
+						}
+					}
+					joueurs[numJoueurs].setPosition(resultat[resultat.length-1][0], resultat[resultat.length-1][1]);
+				}
+				IG.miseAJourAffichage();
+				IG.pause(1500);
+				for(int i=0;i<7;i++){
+					for (int j=0; j<7;j++){
+						IG.supprimerBilleSurPlateau(i, j, 1, 1);
+					}
+				}
+				IG.miseAJourAffichage();
+
+				if(joueurs[numJoueurs].getPosLigne() == joueurs[numJoueurs].getObjetsJoueur()[joueurs[numJoueurs].getNombreObjetsRecuperes()].getPoslePlateau() && joueurs[numJoueurs].getPosColonne() == joueurs[numJoueurs].getObjetsJoueur()[joueurs[0].getNombreObjetsRecuperes()].getPosconnePlateau()){
+					joueurs[numJoueurs].getObjetsJoueur()[joueurs[numJoueurs].getNombreObjetsRecuperes()].enleveDuPlateau();
+					IG.enleverObjetPlateau(joueurs[numJoueurs].getPosLigne(), joueurs[numJoueurs].getPosColonne());
+					IG.changerObjetJoueurAvecTransparence(numJoueurs, joueurs[numJoueurs].getObjetsJoueur()[joueurs[numJoueurs].getNombreObjetsRecuperes()].getNumeroObjet(),joueurs[numJoueurs].getNombreObjetsRecuperes());
+					joueurs[numJoueurs].recupererObjet();
+				}
+
+				if(joueurs[numJoueurs].getNombreObjetsRecuperes() == joueurs[numJoueurs].getObjetsJoueur().length){
+					gagnant = true;
+					if (nbJoueurs==3){
+						if (numJoueurs == 0 || numJoueurs == 1){
+							break;
+						}
+					}else{
+						if (numJoueurs == 0){
+							break;
+						}
+					}
+					
+				}
+			}
+		}
+		for (int numJoueurs=0; numJoueurs<nbJoueurs;numJoueurs++){
+			if(joueurs[numJoueurs].getNombreObjetsRecuperes() == joueurs[numJoueurs].getObjetsJoueur().length){
+				IG.afficherGagnant(numJoueurs);
+			}
 		}
 	}
+
 
 	/**
 	 * 
